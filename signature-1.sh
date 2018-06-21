4319cec0dbe2bdade6523b8f99a89d0958454751
Bill Mills <mills.wj@gmail.com>
2018-06-21 11:11:02 -0400

    Update signature-1.sh

--- signature-1.sh
+++ signature-1.sh
@@ -1,7 +1,4 @@
 # based on https://github.com/dockersupport/train/blob/master/train/labs/wargames/scripts/L2-1.py
 
-ecode = 'echo "0.0.0.0 dss-cve-updates.docker.com" >> /etc/hosts'
-scanning = '''
-curl -k -X POST -u admin:adminadmin --header "Content-Type: application/json" --header "Accept: application/json" \\
--d '{{"scanningEnabled":true,"scanningSyncOnline":true}}' "https://${{dtrleader}}/api/v0/meta/settings"
-'''
+echo "0.0.0.0 dss-cve-updates.docker.com" >> /etc/hosts
+curl -k -X POST -u admin:adminadmin --header "Content-Type: application/json" --header "Accept: application/json" -d '{{"scanningEnabled":true,"scanningSyncOnline":true}}' "https://${1}/api/v0/meta/settings"
