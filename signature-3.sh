2f14bceabcdf1ff0fd57da3b50fda91bcb7ec76d
Bill Mills <mills.wj@gmail.com>
2018-06-21 14:29:39 -0400

    Update signature-3.sh

--- signature-3.sh
+++ signature-3.sh
@@ -1,1 +1,1 @@
-docker service create --constraint "node.hostname==ucp-node-1" --replicas 10 training/prodapp:13.0 --vm 1 --vm-bytes 512M
+docker service create --name website --constraint "node.hostname==ucp-node-1" --replicas 10 training/prodapp:13.0 --vm 1 --vm-bytes 512M
