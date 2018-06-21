53ac398c83df56855b291f066bba4ffddff995e8
Bill Mills <mills.wj@gmail.com>
2018-06-21 12:36:20 -0400

    Update signature-1.sh

--- signature-1.sh
+++ signature-1.sh
@@ -1,4 +1,1 @@
-# based on https://github.com/dockersupport/train/blob/master/train/labs/wargames/scripts/L2-1.py
-
-echo "0.0.0.0 dss-cve-updates.docker.com" >> /etc/hosts
-curl -k -X POST -u admin:adminadmin --header "Content-Type: application/json" --header "Accept: application/json" -d '{"scanningEnabled":true,"scanningSyncOnline":true}' "https://${1}:4443/api/v0/meta/settings" >/dev/null 2>&1
+docker container stop $(docker container ls -q --filter name=dtr-nginx)
