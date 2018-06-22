5256ee2712965485d646013599b7b2672cf5f39e
Bill Mills <mills.wj@gmail.com>
2018-06-22 11:56:41 -0400

    Update signature-2.sh

--- signature-2.sh
+++ signature-2.sh
@@ -2,4 +2,5 @@ sudo iptables -A OUTPUT -p tcp --destination-port 2377 -j DROP
 
 # solution
 # Swarm's management plane has been blocked on the worker, causing tasks to fail scheduling there. 
+# See this with a port test with `nc` or `nmap`
 # Remove this iptables rule and things should be back to normal
