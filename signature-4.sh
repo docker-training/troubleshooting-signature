f174e9d47437de62bd0177ac542118151a21f011
Bill Mills <mills.wj@gmail.com>
2018-06-22 12:02:13 -0400

    Update signature-4.sh

--- signature-4.sh
+++ signature-4.sh
@@ -1,1 +1,8 @@
 sudo date +%Y%m%d -s "20170101"
+
+# solution
+# clock skew is making certs on this node appear invalid; this will produce the 502 bad gateway error in DTR's frontent,
+# and make UCP's ui unreachable if you're hitting that from the same node.
+# acceptable answer: correct the system time on this node by hand, but note if you don't get the times synced perfectly,
+# UCP will throw warnings about clock skew.
+# better solution: install ntp on all nodes, and make sure all certs validate once clocks sync.
