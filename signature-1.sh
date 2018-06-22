2d9d6cbe1dcf3edc1d7f5a883e2db04e0c5dc84a
Bill Mills <mills.wj@gmail.com>
2018-06-22 11:55:34 -0400

    Update signature-1.sh

--- signature-1.sh
+++ signature-1.sh
@@ -1,4 +1,5 @@
 docker container stop $(docker container ls -q --filter name=dtr-nginx)
 
 # solution
-# dtr's nginx reverse proxy container stopped; restart it and things should be back to normal.
+# dtr's nginx reverse proxy container stopped; see this with a simple docker container ls on the DTR node
+# restart it and things should be back to normal.
