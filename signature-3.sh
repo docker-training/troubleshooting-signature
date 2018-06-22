f3171d8118965d1c7ad5879ed7de85f755e6228c
Bill Mills <mills.wj@gmail.com>
2018-06-22 11:55:01 -0400

    Update signature-3.sh

--- signature-3.sh
+++ signature-3.sh
@@ -1,1 +1,6 @@
 docker service create --name website --constraint "node.hostname==ucp-node-1" --replicas 10 training/prodapp:13.0 --vm 1 --vm-bytes 512M
+
+# solution
+# ucp-node-1 has been overprovisioned; see this with docker stats or system tooling. 
+# Acceptable answer would be to schedule less replicas,
+# better answer would be to relax the scheduling constraint and / or provision more nodes
